35b63e4c8c62debf4efa9fb51d1ba3e7fd0ef6a1
kitnarong <kitnarong.wo.62@gmail.com>
2022-01-03 12:37:11 +0700

    UploadToGit

---
+++ nuxt.config.js
@@ -0,0 +1,100 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+
+  dir: {
+    middleware: 'middleware',
+  },
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - trello-clone',
+    title: 'trello-clone',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/styles.scss',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '~/plugins/filters.js'
+  ],
+   router: {
+     middleware: ['auth']
+   },
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: "AIzaSyAomu27HX74eRlFcianVoEN5-VjtqHXjiM",
+          authDomain: "clone-trello-bdad2.firebaseapp.com",
+          projectId: "clone-trello-bdad2",
+          storageBucket: "clone-trello-bdad2.appspot.com",
+          messagingSenderId: "985523844930",
+          appId: "1:985523844930:web:3a74bf1fe47f0bfe37fadb",
+          measurementId: "G-ZL3EW4MYRY"
+
+        },
+        services: {
+          auth: {
+            persistence: 'local', // default
+            initialize: {
+              onAuthStateChangedAction: 'onAuthStateChangedAction',
+              subscribeManually: false
+            },
+            ssr: false, 
+          },
+          firestore: true,
+          storage: true
+        }
+      }
+    ]
+
+  ],
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ plugins/filters.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import moment from 'moment'
+Vue.filter('formatDate', (value) => {
+    if (!value) return ''
+    return moment(value).format('dddd, MMMM Do YYYY')
+})
